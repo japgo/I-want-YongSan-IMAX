4d639c24d180630bf55565f95affbd626754acdf
japgo <japgo@naver.com>
2021-11-09 23:58:05 +0900

    최종 본

--- cgv.py
+++ cgv.py
@@ -1,35 +1,75 @@
 from selenium import webdriver
 import time
+import os
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Chrome Driver를 이용하여 그룹웨어 접근.
+
+import slack_token
+import slack_sdk
+
+BOT_ID, BOT_TOKEN, CHANNEL_NAME = slack_token.get_bot_id_and_token_and_channel()
+bot = slack_sdk.web.client.WebClient( token = BOT_TOKEN )
+resp = bot.conversations_list()
+channel_list = resp[ 'channels' ]
+for channel in channel_list :
+	# print( "channel : ", channel )
+	if channel[ 'name' ] == CHANNEL_NAME :
+		ch_info = channel
+
+ch_id = ch_info[ 'id' ]
+
+
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+
+local_test = False
+
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless') # 창 없는 모드 ( no window )
+url = 'http://www.cgv.co.kr/ticket/?MOVIE_CD=20027683&MOVIE_CD_GROUP=20027683'
+options.add_argument('--headless')
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
-driver = webdriver.Chrome( './chromedriver.exe', options=options )
-url = 'http://www.cgv.co.kr/ticket/?MOVIE_CD=20027683&MOVIE_CD_GROUP=20027683'
-driver.get( url )
-print( driver.page_source )
-# //*[@id="theater_area_list"]/ul/li[1]/div/ul/li[24]/a/text()
-# <a href="#" onclick="theaterListClickListener(event);return false;">용산아이파크몰<span class="sreader"></span></a>
-# theater_area_list > ul > li.selected > div > ul > li:nth-child(24) > a
-# document.querySelector("#theater_area_list > ul > li.selected > div > ul > li:nth-child(24) > a")
+
+
+if local_test :
+	driver = webdriver.Chrome( './chromedriver.exe', options=options )
+	driver.get( url )
+
+else:
+	chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+	options.binary_location = chrome_bin
+	driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
+	driver.get( url )
+
+
 
 ticket_iframe = driver.find_element( by=By.CSS_SELECTOR, value="iframe[id='ticket_iframe']")
 driver.switch_to.frame( ticket_iframe )
 theater_area_list = driver.find_element( by=By.CSS_SELECTOR, value="div[id='theater_area_list']" )
+time.sleep( 3 )
 yongsan = theater_area_list.find_element( by=By.CSS_SELECTOR, value="li[data-index='119']" )
+#yongsan = WebDriverWait( driver, 5 ).until( EC.element_to_be_clickable( ( By.CSS_SELECTOR, "li[data-index='119']" ) ) )
 yongsan.click()
 
+
 date_list = driver.find_element( by=By.CSS_SELECTOR, value="div[id='date_list']")
+time.sleep( 1 )
 IwantThisDay = date_list.find_element( by=By.CSS_SELECTOR, value="li[date='20211113']" )
-IwantThisDay.click()
+#IwantThisDay = WebDriverWait( date_list, 5 ).until( EC.element_to_be_clickable( ( By.CSS_SELECTOR, "li[date='20211113']" ) ) )
 
-time_list_nano = driver.find_element( by=By.CSS_SELECTOR, value="div[class='section section-time']" )
+while True:
+	IwantThisDay.click()
 
-
-time.sleep( 3 )
+	section_section_time = driver.find_element( by=By.CSS_SELECTOR, value="div[class='section section-time']" )
+	col_body = section_section_time.find_element( by=By.CSS_SELECTOR, value="div[class='col-body']" )
+	content_scroll_y = col_body.find_element( by=By.CSS_SELECTOR, value="div[class='content scroll-y']" )
+	theaters = content_scroll_y.find_elements( by=By.CSS_SELECTOR, value="div[class='theater']" )
+	for theater in theaters :
+		name = theater.find_element( by=By.CSS_SELECTOR, value="span[class='name']" )
+		if( str( name.text ).find( "IMAX" ) >= 0 ):
+			bot.chat_postMessage( channel=ch_id, text="떳다 IMAX!!!!! " + name.text )
+		
+	time.sleep( 5 )
---
+++ slack_token.py
@@ -0,0 +1,7 @@
+
+def get_bot_id_and_token_and_channel():
+	return 'U02DCLSTTTJ', 'xoxb-283900351073-2454706945936-eubtGkrH0Q0Cf75zVRyhrodk', 'general' #JandW
+	#return 'U02HXCANAS1', 'xoxb-581212750160-2609418758885-tTUNSH348Qv5KImnT0oXfbsD', '_평일휴일_ot_신청' #sw3team
+
+def get_app_level_token():
+	return 'xapp-1-A02CG9UKBM4-2441617197367-96ad2a178bccf125637d98ff48585b35c796c1a7d17ef77e7ecc7801ca44e95f' #JandW
